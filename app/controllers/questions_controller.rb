9f099711fc3ac8ec5e9dcb64161527901668065d
Walsh Costigan <walshcostigan@gmail.com>
2017-03-03 23:26:07 -0500

    added a few seed files

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
 
   # GET /questions
   def index
-    @questions = Question.all
+    @questions = Question.order("RANDOM()").limit(1)
 
     render json: @questions
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create(question: "Would you rather fight 1 horse-sized duck, or 100 duck-sized horses?")
+Question.create(question: "Which would you prefer — three wishes over five years or one wish right now?")
+Question.create(question: "Would you rather be clever or beautiful?")
+Question.create(question: "Would you rather be really hairy or bald?")
+Question.create(question: "Would you like to be taller or thinner?")
+Question.create(question: "Would you rather go to a tropical island or the Arctic")
